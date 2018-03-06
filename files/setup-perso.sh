3b11b71423dc8fc2ec357664c8c1db5ce29bc69e
Julien Palmas <julien.palmas@gmail.com>
2018-03-06 15:52:21 +0100

    start setup perso

---
+++ files/setup-perso.sh
@@ -0,0 +1,5 @@
+echo "Installing ZSH"
+sudo apt-get install zsh
+
+echo "Installing Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
